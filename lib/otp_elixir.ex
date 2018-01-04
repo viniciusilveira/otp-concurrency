356f61a172dae79f99b8465c4ac5bb6d058cefbf
Marcos Vinicius O. Silveira <viniciussilveira6@gmail.com>
2018-01-04 17:42:56 -0200

    Add handle_call example

--- lib/otp_elixir.ex
+++ lib/otp_elixir.ex
@@ -1,18 +1,30 @@
 defmodule OtpElixir do
-  @moduledoc """
-  Documentation for OtpElixir.
-  """
+  use GenServer
+
+  ### GenServer API
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> OtpElixir.hello
-      :world
-
+  GenServer.init/1 callback
   """
-  def hello do
-    :world
+  def init(state), do: {:ok, state}
+
+  @doc """
+  GenServer.handle_call/3 callback
+  """
+  def handle_call(:dequeue, _from, [value | state]) do
+    {:reply, value, state}
   end
+
+  def handle_call(:dequeue, _from, []), do: {:reply, nil, []}
+
+  def handle_call(:queue, _from, state), do: {:reply, state, state}
+
+  ### Client API / Helper functions
+
+  def start_link(state \\ []) do
+    GenServer.start_link(__MODULE__, state, name: __MODULE__)
+  end
+
+  def queue, do: GenServer.call(__MODULE__, :queue)
+  def dequeue, do: GenServer.call(__MODULE__, :dequeue)
 end
